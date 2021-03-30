5e0017b4d4d3d81b73c1495b05e732b8a2b829c9
jangjaeko <jangjaeko45@gmail.com>
2021-03-30 15:11:20 +0900

    adminlecture

--- src/components/views/ProfessorPage/AdminLecture.js
+++ src/components/views/ProfessorPage/AdminLecture.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import {UserOutlined} from '@ant-design/icons';
 import './Sections/AdminLecture.css'
-import StudentList from './Sections/StudentList'
+import StudentList from './Sections/StudentList';
+import Addstudent from './Sections/Addstudent';
+
 //import Lecturelist from './Sections/Lecturelist';
 function AdimnLecture() {
 
@@ -17,6 +19,10 @@ function AdimnLecture() {
             <div class ="col-md-6 " >
                 <span class="badge badge-danger"  style={{width : "100px", display : 'block', marginBottom : '20px',fontSize : '1rem'}}>강의시간</span> 
                 <span class="badge badge-light"  style={{width : "300px" ,display : 'block' ,fontSize : '1rem'}}>금요일 13:30 ~ 19:00</span>
+                <div >
+                    <Addstudent />
+                </div>
+                <span class="badge badge-danger mt-4"  style={{width : "100px", display : 'block', marginBottom : '20px',fontSize : '1rem'}}>저정하기</span> 
             </div>
         </div>
     )
---
+++ src/components/views/ProfessorPage/Sections/Addstudent.js
@@ -0,0 +1,118 @@
+import React ,{useState}from 'react'
+import './Lecturelist.css' ;
+import { faCheckSquare, faSpinner } from "@fortawesome/free-solid-svg-icons"
+import { faSquare } from "@fortawesome/free-regular-svg-icons"
+import { MDBIcon} from 'mdbreact';
+function Addstudent() {
+ 
+
+    return (
+        <div class="container overflow-auto" style ={{height :'500px'}} >
+    <div class="row d-flex mt-4 ">
+        <div class="col" style ={{marginLeft :"-15px"}}>
+            <div class="card">
+                <div class="d-flex justify-content-between align-items-center"> <h4 class="font-weight-bold"> 출석확인</h4>
+                </div>
+                <div>
+                <span class="badge badge-light"  style={{width : "130px" ,height:"30px",display : 'block' ,fontSize : '1rem',justifyContent:'center',paddingTop:'8px'}}>16012416 장재호</span>    
+                </div>
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-row align-items-center"> <span style={{fontWeight:'bold'}} >1주차</span>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span class="badge badge-success" style={{fontSize : '1rem'}}>출석</span></div>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >지각</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >결석</span> </div> 
+                        </div> 
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-row align-items-center"> <span style={{fontWeight:'bold'}} >2주차</span>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span class="badge badge-success" style={{fontSize : '1rem' }}>출석</span></div>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >지각</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >결석</span> </div> 
+                        </div> 
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-row align-items-center"> <span style={{fontWeight:'bold'}} >3주차</span>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span class="badge badge-success" style={{fontSize : '1rem' }}>출석</span></div>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >지각</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >결석</span> </div> 
+                        </div> 
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-row align-items-center"> <span style={{fontWeight:'bold'}} >4주차</span>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span class="badge badge-success" style={{fontSize : '1rem'}}>출석</span></div>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >지각</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >결석</span> </div> 
+                        </div> 
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-row align-items-center"> <span style={{fontWeight:'bold'}} >5주차</span>
+                            <div class="d-flex flex-column align-items-center" style={{width: '100px'}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >출석</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span class="badge badge-warning" style={{fontSize : '1rem' }}>지각</span></div>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >결석</span> </div> 
+                        </div> 
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-row align-items-center"> <span style={{fontWeight:'bold'}} >6주차</span>
+                            <div class="d-flex flex-column align-items-center align-items-center" style={{width: "100px"}}> <span class="badge badge-success  " style={{fontSize : '1rem'}}>출석</span></div>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >지각</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >결석</span> </div> 
+                        </div> 
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-row align-items-center"> <span style={{fontWeight:'bold'}} >7주차</span>
+                            
+                            <div class="d-flex flex-column align-items-center" style={{width:"100px"}} > <span style={{fontSize: '1rem', fontWeight:'bold', textAlign :'center'}} >출석</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >지각</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span class="badge badge-danger" style={{fontSize : '1rem'}}>결석</span></div>
+                        </div> 
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-row align-items-center"> <span style={{fontWeight:'bold'}} >8주차</span>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span class="badge badge-success" style={{fontSize : '1rem'}}>출석</span></div>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >지각</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >결석</span> </div> 
+                        </div> 
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-row align-items-center"> <span style={{fontWeight:'bold'}} >9주차</span>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span class="badge badge-success" style={{fontSize : '1rem'}}>출석</span></div>
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >지각</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "100px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >결석</span> </div> 
+                        </div> 
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex flex-row align-items-center"> <span style={{fontWeight:'bold'}} >10주차</span>
+                            <div class="d-flex flex-column align-items-center" style={{width:'80px'}}> <span class="badge badge-success" style={{fontSize : '1rem' }}>출석</span></div>
+                            <div class="d-flex flex-column align-items-center" style={{width: "120px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >지각</span> </div> 
+                            <div class="d-flex flex-column align-items-center" style={{width: "80px"}}> <span style={{fontSize: '1rem', fontWeight:'bold'}} >결석</span> </div> 
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    )
+}
+
+export default Addstudent
